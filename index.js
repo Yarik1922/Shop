955ad1a5bb1f08d2f0e8a44fae6fc46f4d056e92
Yarik1922 <117753092+Yarik1922@users.noreply.github.com>
2022-12-01 00:10:53 +0200

    Add files via upload
    sime app with 3 div and cart

---
+++ index.js
@@ -0,0 +1,85 @@
+const shop = document.querySelector(".shop__items");
+const cart = document.querySelector(".cart__list");
+const items = [
+  {
+    id: 1,
+    name: "Sony Playstation",
+    price: 3300,
+    image:
+      "https://d3m9l0v76dty0.cloudfront.net/system/photos/6684441/large/a408dc9b8bd1fc5098d1bc7c9d6a07b9.webp",
+  },
+  {
+    id: 2,
+    name: "Xbox One",
+    price: 3000,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgmsdv4AbNQp3Pn7EEZv83XEX1XeyI701ak-4HydxPpNd3GtPw6Dvt9VAkVGQqoQKKNEs&usqp=CAU",
+  },
+  {
+    id: 3,
+    name: "Nintendo Switch",
+    price: 1200,
+    image:
+      "https://www.lior-pc.co.il/wp-content/uploads/2021/08/Nintendo-Switch1.png",
+  },
+];
+const inCart = [];
+for (let i = 0; i < items.length; i++) {
+  const li = document.createElement("li");
+  li.className = "shop__item";
+  li.innerHTML = `<div class="shop__item-imgBox">
+  <img
+    src="${items[i].image}"
+    class="shop__item-Img"
+    alt=""
+  />
+</div>
+<div class="shop__item-content">
+  <h3 class="shop__item-title">${items[i].name}</h3>
+  <span class="shop__item-price">$${items[i].price}</span>
+  <div class="shop__item-info">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Porro, consectetur.
+    </p>
+  </div>
+  <button onclick="addToCart(${i})" class="shop__item-btn" type="button">
+    Add to Cart
+  </button>
+</div>`;
+  shop.append(li);
+}
+function addToCart(id) {
+  if (inCart.length > 0) {
+    const check = inCart.findIndex((item) => item.id === items[id].id);
+    if (check === -1) {
+      inCart.push(items[id]);
+    }
+  } else {
+    inCart.push(items[id]);
+  }
+  createCart();
+}
+function createCart() {
+  cart.innerHTML = "";
+  for (let i = 0; i < inCart.length; i++) {
+    const li = document.createElement("li");
+    li.className = "cart__item";
+    li.innerHTML = `
+                    <div class="cart__item-info">
+                    <h3 class="cart__item-name">${inCart[i].name}</h3>
+                    <span class="cart__item-price">$${inCart[i].price}</span>
+                    </div>
+                        <button
+                          class="cart__item-remove"
+                        onclick="remove(${i})"
+>
+                          &times;
+                        </button>`;
+    cart.append(li);
+  }
+}
+function remove(index) {
+  inCart.splice(index, 1);
+  createCart();
+}
